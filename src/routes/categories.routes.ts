5187a5b053237491f4c3b397a9128558929f9bac
Rafael Santana <rafaelsantana.devweb@gmail.com>
2021-03-28 10:07:17 -0300

    :art: improvement/ Change project structure

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import CategoriesRepository from "../modules/cars/repositories/CategoriesRepository";
-import CreateCategoryService from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
+
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.get("/", (request, response) => {
@@ -13,13 +14,7 @@ categoriesRoutes.get("/", (request, response) => {
 });
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 export default categoriesRoutes;
