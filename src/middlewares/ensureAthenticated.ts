aff0b18e3e657f826e2cc216b1c04d3bb1156bfa
Rafael Santana <rafaelsantana.devweb@gmail.com>
2021-05-01 12:42:30 -0300

    :art: improvement/ Custom display errors

--- src/middlewares/ensureAthenticated.ts
+++ src/middlewares/ensureAthenticated.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
 
-interface IPayload {
+interface ITokenPayload {
   user_id: string;
 }
 
@@ -11,11 +12,11 @@ export async function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction
-) {
+): Promise<void> {
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error("Token missing!");
+    throw new AppError("Token missing!", 401);
   }
 
   const [, token] = authHeader.split(" ");
@@ -24,18 +25,18 @@ export async function ensureAuthenticated(
     const { user_id } = verify(
       token,
       "88faa79357f4c8e308162b2aece7259c"
-    ) as IPayload;
+    ) as ITokenPayload;
 
     const usersRepository = new UsersRepository();
 
     const user = await usersRepository.findById(user_id);
 
     if (!user) {
-      throw new Error("User does not exists!");
+      throw new AppError("User does not exists!", 401);
     }
 
-    next();
+    return next();
   } catch {
-    throw new Error("Invalid token!");
+    throw new AppError("Invalid token!", 401);
   }
 }
